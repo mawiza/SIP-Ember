15fe07efc2def47e8176d0a2ca0a63194f74860f
William Miles <william@miles.dk>
2014-06-08 18:40:07 +0200

    Added coverage to karma

--- karma.conf.js
+++ karma.conf.js
@@ -35,17 +35,23 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    //
     preprocessors: {
+        "**/*.handlebars": ['ember'],
         '**/*.coffee': ['coffee'],
-        "**/*.handlebars": 'ember'
+        '**/js/app.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
+    coverageReporter: {
+        type : 'html',
+        dir : 'coverage/'
+    },
 
     // web server port
     port: 9876,
