85c5b9694e0db36e4b6189320ab4052000323593
William Miles <william@miles.dk>
2014-06-11 09:32:51 +0200

    Generated template file

---
+++ js/app/templates.js
@@ -0,0 +1,45 @@
+Ember.TEMPLATES['about'] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  
+
+
+  data.buffer.push("<div>This is a template</div>");
+  
+});
+Ember.TEMPLATES['focusareas'] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  
+
+
+  data.buffer.push("<table class=\"table\">\r\n    <thead>\r\n    <tr>\r\n        <th style=\"width:200px\">Focus areas</th>\r\n        <th>Description</th>\r\n        <th style=\"width: 240px; text-align:center;\">Action</th>\r\n    </tr>\r\n    </thead>\r\n    <tbody>\r\n    <tr>\r\n        <td style=\"width:200px\">data</td>\r\n        <td>data</td>\r\n        <td style=\"width: 240px; text-align:center;\" class=\"pull-right\">\r\n            <button class=\"btn btn-default\">View</button>\r\n            <button class=\"btn btn-default\">Edit</button>\r\n            <button class=\"btn btn-danger\">Delete</button>\r\n        </td>\r\n    </tr>\r\n    </tbody>\r\n</table>");
+  
+});
+Ember.TEMPLATES['index'] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  
+
+
+  data.buffer.push("<h2>Welcome to Ember.js :)</h2>\r\n<div class=\"description\">Text</div>");
+  
+});
+Ember.TEMPLATES['administrations'] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  
+
+
+  data.buffer.push("<table class=\"table\">\r\n    <thead>\r\n    <tr>\r\n        <th>Administration</th>\r\n        <th style=\"width: 240px; text-align:center;\">Action</th>\r\n    </tr>\r\n    </thead>\r\n    <tbody>\r\n    <tr>\r\n        <td>data</td>\r\n        <td style=\"width: 240px; text-align:center;\" class=\"pull-right\">\r\n            <button class=\"btn btn-default\">View</button>\r\n            <button class=\"btn btn-default\">Edit</button>\r\n            <button class=\"btn btn-danger\">Delete</button>\r\n        </td>\r\n    </tr>\r\n    </tbody>\r\n</table>");
+  
+});
+Ember.TEMPLATES['themes'] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  
+
+
+  data.buffer.push("<table class=\"table\">\r\n    <thead>\r\n    <tr>\r\n        <th style=\"width:200px\">Themes</th>\r\n        <th>Description</th>\r\n        <th style=\"width: 240px; text-align:center;\">Action</th>\r\n    </tr>\r\n    </thead>\r\n    <tbody>\r\n    <tr>\r\n        <td style=\"width:200px\">data</td>\r\n        <td>data</td>\r\n        <td style=\"width: 240px; text-align:center;\" class=\"pull-right\">\r\n            <button class=\"btn btn-default\">View</button>\r\n            <button class=\"btn btn-default\">Edit</button>\r\n            <button class=\"btn btn-danger\">Delete</button>\r\n        </td>\r\n    </tr>\r\n    </tbody>\r\n</table>");
+  
+});
